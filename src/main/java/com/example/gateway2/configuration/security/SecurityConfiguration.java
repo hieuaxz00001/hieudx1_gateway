adf915b98b6566a8d16e4d443187b98ce1004c31
hieuaxz00001 <45386604+hieuaxz00001@users.noreply.github.com>
2023-09-18 23:29:37 +0700

    dsfsfdsfs

---
+++ src/main/java/com/example/gateway2/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,106 @@
+package com.example.gateway2.configuration.security;
+
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import io.netty.resolver.DefaultAddressResolverGroup;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cglib.core.Converter;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+@Configuration
+@Slf4j
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+
+        // Enable and configure CORS
+        http.cors(ServerHttpSecurity.CorsSpec::disable);
+
+        // State-less session (state in access-token only)
+
+        // Disable CSRF because of state-less session-management
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        // Return 401 (unauthorized) instead of 302 (redirect to login) when
+        // authorization is missing or invalid
+//        http.exceptionHandling(eh -> eh.authenticationEntryPoint((request, response, authException) -> {
+//            response.addHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Restricted Content\"");
+//            response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+//        }));
+
+        // @formatter:off
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().permitAll());
+//        http.authorizeExchange(exchanges -> exchanges
+//                .pathMatchers("/user/**").permitAll()
+//                .anyExchange()
+//                .authenticated())
+//                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        // @formatter:on
+        return http.build();
+    }
+
+
+
+   @Bean
+   public SecurityFilterChain springSecurityFilterChain(HttpSecurity httpSecurity, ServerLogoutSuccessHandler handler) throws Exception {
+       log.info("heheheheheh");
+       httpSecurity.csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(auth ->
+                       auth.requestMatchers(HttpMethod.GET,"/actuator/health/readiness", "/actuator/health/liveness", "/v3/api-docs/**","/public").permitAll()
+                               .anyRequest().authenticated()
+               ).sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+       return httpSecurity.build();
+   }
+
+
+}
